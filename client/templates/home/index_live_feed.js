3f4797ce4417d17f910eff113b2f0b6715c631fb
Dominic Ho <domtkho@gmail.com>
2015-03-18 17:34:01 +0800

    Added camera.

--- client/templates/home/index_live_feed.js
+++ client/templates/home/index_live_feed.js
@@ -7,7 +7,7 @@ Template.indexLifeFeed.helpers({
 Template.indexLifeFeed.events({
   'click [data-activate-camera]': function () {
     MeteoricCamera.getPicture(function(error, data){
-      return data;
+      return error | data;
     });
   }
 });
